2d339cbb010d2c25257462b86deeb8834c3519d1
noiseWizard <64124242+noiseWizard@users.noreply.github.com>
2022-12-13 11:01:32 +1100

    Update Counting Cards.js
    i wanted to do another attempt before finding the answer through hints, this time practicing switches.
    
    I still didn't get the answer but i think this syntax is correct, it's just not giving the right answer.

--- 0b. Counting Cards/Counting Cards.js
+++ 0b. Counting Cards/Counting Cards.js
@@ -1,23 +1,35 @@
 let count = 0;
 
 function cc(card) {
-  // Only change code below this line
+    // Only change code below this line
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count + 1;
+            break;
+        case 7:
+        case 8:
+        case 9:
+            count + 0;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count - 1;
+            break;
+    }
 
-  if (card <= 6) {
-    count = count + 1;
-  } else if (card >= 7 && card <= 9){
-    count = count + 0;
-  } else if (card = 10 || card == 'J' || card == 'Q' || card == 'K' || card == 'A' ) {
-    count = count - 1;
-  }
-
-  if (count <= 0) {
-  return card + " Hold";
-  } else
-  {
-    return card + " Bet";
-  }
-  // Only change code above this line
+    if (count <= 0) {
+        return count + " Hold";
+    } else {
+        return count + " Bet";
+    }
+    // Only change code above this line
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
